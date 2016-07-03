6c239508af7aa8134a7030581345b5a76708861a
Michal Holasek <isindzr@gmail.com>
2016-07-03 19:25:38 +0200

    Use function declarations instead of arrows

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { isFunction, validate } from './lib/utils';
 
-export const compose = (...callbacks) => {
-  const composite = (...args) => {
+export function compose(...callbacks) {
+  function composite(...args) {
     const initialValue = callbacks[callbacks.length - 1](...args);
     return callbacks.reduceRight((previousResult, callback) => {
       return callback(previousResult);
     }, initialValue);
-  };
+  }
 
   const isValid = (callbacks.length !== 0 && validate(callbacks, isFunction));
 
@@ -15,31 +15,31 @@ export const compose = (...callbacks) => {
   }
 
   return composite;
-};
+}
 
-export const curry = (fn) => {
-  const createCurry = (...boundArgs) => {
-    return (...currentArgs) => {
+export function curry(fn) {
+  function createCurry(...boundArgs) {
+    return function boundFunction(...currentArgs) {
       const args = boundArgs.concat(currentArgs);
       return args.length < fn.length ? createCurry(...args) : fn(...args);
     };
-  };
+  }
 
   if (!isFunction(fn)) {
     throw new Error('Supplied argument is not a function.');
   }
 
   return createCurry();
-};
+}
 
-export const partial = (fn, ...boundArgs) => {
-  const boundFunction = (...args) => {
+export function partial(fn, ...boundArgs) {
+  function boundFunction(...args) {
     return fn(...boundArgs.concat(args));
-  };
+  }
 
   if (!isFunction(fn)) {
     throw new Error('First argument is not a function.');
   }
 
   return boundFunction;
-};
+}
